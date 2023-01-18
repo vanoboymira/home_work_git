39208290927fdbb30bfe3752c240e63b590cb69f
vanoboymira <118725949+vanoboymira@users.noreply.github.com>
2023-01-18 15:27:01 +0300

    added var = 10

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -3,9 +3,8 @@ import org.junit.jupiter.api.Test;
 
 public class NewTest {
     @Test
-    
     void newTest(){
-        int i = 3;
+        int i = 10;
         System.out.println("This is the best Test");
         Assertions.assertTrue(i>2);
     }
