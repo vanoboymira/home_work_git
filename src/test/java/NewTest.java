218df38c6c9451dbf8354fca0f48e18fcbd85230
vanoboymira <118725949+vanoboymira@users.noreply.github.com>
2023-01-18 14:25:16 +0300

    added var

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -3,8 +3,9 @@ import org.junit.jupiter.api.Test;
 
 public class NewTest {
     @Test
+    int i = 3;
     void newTest(){
-        Assertions.assertTrue(3>2);
+        Assertions.assertTrue(i>2);
     }
 
 }
