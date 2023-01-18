96c6afd5df7547c919a702127fe798b2922df896
vanoboymira <118725949+vanoboymira@users.noreply.github.com>
2023-01-18 14:13:34 +0300

    added sout

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 public class NewTest {
     @Test
     void newTest(){
+        System.out.println("This is the best Test");
         Assertions.assertTrue(3>2);
     }
 
