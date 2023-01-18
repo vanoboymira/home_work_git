2958aa61154af7d2d75bfe2e5909cf37ea6b34d9
vanoboymira <118725949+vanoboymira@users.noreply.github.com>
2023-01-18 13:42:52 +0300

    new test added

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NewTest {
+    @Test
+    void newTest(){
+        Assertions.assertTrue(3>2);
+    }
+
+}
